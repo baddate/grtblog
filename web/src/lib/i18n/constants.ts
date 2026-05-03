0b69880b14113c3ec6ca2717d955fc6a4c687019
Mercas <me@smj.im>
2026-05-03 18:37:26 +0800

    refactor(i18n): replace hardcoded constants with registry re-exports

---
+++ web/src/lib/i18n/constants.ts
@@ -0,0 +1,10 @@
+export {
+  SUPPORTED_LANGS,
+  DEFAULT_LANG,
+  FALLBACK_LANG,
+  LANGUAGES,
+  NON_DEFAULT_LANGS,
+  getLanguageMeta,
+  isSupportedLang,
+} from './languages';
+export type { LanguageMeta } from './languages';
